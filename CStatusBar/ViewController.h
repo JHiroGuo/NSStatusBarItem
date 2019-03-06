395e93eeb2defb9bec1ffbdc6fd4ae09c101eb83
gjh <jh_guo@tiertime.net>
2019-03-06 10:24:25 +0800

    Initial Commit

---
+++ CStatusBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CStatusBar
+//
+//  Created by GJH on 2019/3/6.
+//  Copyright © 2019 GJH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ CStatusBar/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CStatusBar
+//
+//  Created by GJH on 2019/3/6.
+//  Copyright © 2019 GJH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
